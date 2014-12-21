76af8cb161d4c5ea561a5c52ee89c7001526b2ab
StephGbzh <StephGbzh@users.noreply.github.com>
2014-12-21 14:26:22 +0100

    Update SwordsAndPotions2WikiInKongregateChat.meta.js

--- SwordsAndPotions2WikiInKongregateChat.meta.js
+++ SwordsAndPotions2WikiInKongregateChat.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name Swords & Potions 2 Wiki Bot for Kongregate chat
-// @namespace *
+// @name        Swords & Potions 2 Wiki Bot for Kongregate chat
+// @namespace   *
 // @description Adds a small input box under the chat to request items definition from the Swords & Potions 2 wiki
-// @include http://www.kongregate.com/games/EdgebeeStudios/swords-and-potions-2*
-// @version 1.02
-// @grant GM_xmlhttpRequest
-// @grant GM_log
-// @grant GM_addStyle
-// @author Stef
-// @updateURL https://raw.githubusercontent.com/StephGbzh/SwordsAndPotions2WikiInKongregateChat/master/SwordsAndPotions2WikiInKongregateChat.meta.js
+// @include     http://www.kongregate.com/games/EdgebeeStudios/swords-and-potions-2*
+// @version     1.03
+// @grant       GM_xmlhttpRequest
+// @grant       GM_log
+// @grant       GM_addStyle
+// @author      Stef
+// @updateURL   https://raw.githubusercontent.com/StephGbzh/SwordsAndPotions2WikiInKongregateChat/master/SwordsAndPotions2WikiInKongregateChat.meta.js
 // @downloadURL https://raw.githubusercontent.com/StephGbzh/SwordsAndPotions2WikiInKongregateChat/master/SwordsAndPotions2WikiInKongregateChat.user.js
 // ==/UserScript==
